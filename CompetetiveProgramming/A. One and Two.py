32a9baec237b8cf9d7d3ba246de88ec756ab0ebb
Esteban <111403759+tebanspam11@users.noreply.github.com>
2025-09-18 15:29:31 -0500

    Create A. One and Two.py

---
+++ CompetetiveProgramming/A. One and Two.py
@@ -0,0 +1,33 @@
+#A. One and Two
+import sys
+
+def one_and_two():
+  t = int(sys.stdin.readline())
+  for _ in range(t):
+    n = int(sys.stdin.readline())
+    array = sys.stdin.readline().split()
+    twocount = 0
+
+    #Segun el juez count("2") ocupa mas tiempo y memoria, se opta por recorrido del arreglo normal
+
+    for i in array:
+      if i == "2":
+        twocount += 1
+
+    if twocount == 0:
+      print(1)
+      continue
+
+    if twocount %2 == 1:
+      print(-1)
+      continue
+
+    index = 0
+    for i, x in enumerate(array, 1):
+      if x == "2":
+        index += 1
+        if index == (twocount//2):
+          print(i)
+          break
+
+one_and_two()
